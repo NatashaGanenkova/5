fd51bd93becbb0909f21ba2bd01bf7e04a9368a9
Nata <ganenkova@mail.ru>
2023-02-15 20:18:49 +0300

    Задача34

---
+++ zadacha34/Program.cs
@@ -0,0 +1,21 @@
+﻿int[]array =GetArray(5);
+Console.WriteLine(String.Join(" ",array));
+Console.WriteLine(Getcount(array));
+int[] GetArray(int size)
+{
+    int[]array=new int[size];
+    for(int i=0;i<array.Length;i++){
+    array[i]=new Random().Next(100,1000);
+    }
+return array;
+}    
+int Getcount (int[]array){
+    int rezult=0;
+    for(int j=0;j<array.Length;j++){
+        if((array[j]%2)==0){
+        rezult++;
+        }
+        }
+        return rezult;
+
+}
