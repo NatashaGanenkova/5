244642aa12595863f97cc87e98f9909628fa09d1
Nata <ganenkova@mail.ru>
2023-02-16 10:52:15 +0300

    Задача38

---
+++ zadacha38/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("Введите элементы массива через пробел: ");
+string elements = Console.ReadLine();
+int[] baseArray = GetArrayFromString(elements);
+
+Console.WriteLine(String.Join(" ", ResultArray(baseArray)));
+
+int[] GetArrayFromString(string stringArray)
+{
+    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] res = new int[nums.Length];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        res[i] = int.Parse(nums[i]);
+    }
+    return res;
+}
+
+int ResultArray(int[] array)
+{
+ int min = int.MaxValue;
+ int max = int.MinValue;   
+    for (int i = 0; i < array.Length ; i++)
+    {
+        if (min>array[i]){
+            min=array[i];
+        }
+        if (max<array[i]){
+            max=array[i];
+        }
+    }
+   
+    return max-min;
+}
