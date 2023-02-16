ea821ecfd90a3a7eb303c38a0829c8d772068e61
Nata <ganenkova@mail.ru>
2023-02-16 10:30:40 +0300

    Задача36

---
+++ zadacha36/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Введите длину массива :");
+int num=int.Parse(Console.ReadLine());
+int[]array = GetBinArray(num);
+Console.WriteLine(String.Join(" ",array));
+Console.Write($"[{CountOfEven(array)}]");
+
+int[]GetBinArray(int size)
+{
+      int[]result =new int[size];
+      for(int i=0;i<size;i++){
+      result[i]=new Random().Next(1,24);
+     }
+      return result;
+}
+int CountOfEven(int[]array){
+    int sum = 0;
+    for(int i=0;i<array.Length;i++)
+    {
+        if(i%2==1){
+        sum=sum+array[i];}
+       
+        
+
+    }    
+
+         return sum;
+}
+       
